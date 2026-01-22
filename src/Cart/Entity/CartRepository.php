b1ef698f6c2a932a8dcb4a0f9f24ebf3d148383d
Flaaim <flaaim@list.ru>
2026-01-22 22:25:51 +1000

    added CartRepository.php

---
+++ src/Cart/Entity/CartRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Cart\Entity;
+
+use App\Shared\Domain\ValueObject\Id;
+
+interface CartRepository
+{
+    public function create(Cart $cart): void;
+    public function find(Id $id): ?Cart;
+}
