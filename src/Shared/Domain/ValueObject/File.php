885840d0502d44657745610d48cc431ba6790641
Flaaim <flaaim@list.ru>
2026-01-26 21:55:37 +1000

    added FileTest

--- src/Shared/Domain/ValueObject/File.php
+++ src/Shared/Domain/ValueObject/File.php
@@ -2,19 +2,34 @@
 
 namespace App\Shared\Domain\ValueObject;
 
+use App\Shared\Domain\Service\Template\TemplatePath;
 use Webmozart\Assert\Assert;
 
 class File
 {
     private string $value;
-    public function __construct(string $pathToFile)
+    public function __construct(string $pathToFile, TemplatePath $templatePath)
     {
         Assert::notEmpty($pathToFile);
-        $this->value = ltrim($pathToFile, '/');
+        $this->value =
+            DIRECTORY_SEPARATOR .
+            trim($templatePath->getValue(), '/') .
+            DIRECTORY_SEPARATOR .
+            trim($pathToFile, '/');
     }
-
-    public function getPathToFile(): string
+    public function getPath(): string
     {
         return $this->value;
+    }
+    public function getFile(): string
+    {
+        if(!$this->exists()){
+            throw new \DomainException("File '{$this->value}' does not exist");
+        }
+        return $this->value;
+    }
+    public function exists(): bool
+    {
+        return file_exists($this->value);
     }
 }
--- src/Shared/Test/Unit/ValueObject/FileTest.php
+++ src/Shared/Test/Unit/ValueObject/FileTest.php
@@ -2,19 +2,55 @@
 
 namespace App\Shared\Test\Unit\ValueObject;
 
+use App\Shared\Domain\Service\Template\TemplatePath;
 use App\Shared\Domain\ValueObject\File;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class FileTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $file = new File('/ppe/template.rar');
-        $this->assertEquals('ppe/template.rar', $file->getPathToFile());
+        $fileName = $this->createFile();
+        $file = new File($fileName, new TemplatePath(sys_get_temp_dir()));
+
+        $this->assertEquals('/tmp/file.txt', $file->getPath());
+        $this->assertTrue($file->exists());
+
+        $this->clearTempDir();
+    }
+
+    #[DataProvider('fileNameProvider')]
+    public function testTrimSlash($file, $root, $expected): void
+    {
+        $file = new File($file, new TemplatePath($root));
+        $this->assertEquals($expected, $file->getPath());
+    }
+
+    public static function fileNameProvider(): array
+    {
+        return [
+          ['file.txt', '/tmp/', '/tmp/file.txt'],
+          ['/file.txt', '/tmp//', '/tmp/file.txt'],
+          ['/file.txt/', '/tmp//', '/tmp/file.txt'],
+          ['//file.txt/', 'tmp', '/tmp/file.txt'],
+        ];
     }
     public function testEmpty(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        new File('');
+        new File('', new TemplatePath(sys_get_temp_dir()));
     }
+
+    private function createFile(): string
+    {
+        $path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'file.txt';
+        file_put_contents($path, 'some_content');
+        return basename($path);
+    }
+    private function clearTempDir(): void
+    {
+        unlink(sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'file.txt');
+    }
+
 }
